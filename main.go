23fd3543213de24292d6ed94ac06afcf24d4db39
David Senack <david.senack@gmail.com>
2025-02-06 09:24:11 -0500

    create test metric data

--- main.go
+++ main.go
@@ -2,6 +2,26 @@ package main
 
 import "fmt"
 
+type metrics struct {
+	Impressions int64 `json:"impressions"`
+	Clicks      int64 `json:"clicks"`
+	Conversions int64 `json:"conversions"`
+}
+
+// ::TODO Add Test Campaign Data
+//type campaigns struct {
+//	Campaigns
+//}
+
+// !!TODO:: Remove Fake Testing Data!
+var fakeMetrics = []metrics{
+	{Impressions: 23832, Clicks: 8244, Conversions: 1281},
+	{Impressions: 374728, Clicks: 4728, Conversions: 918},
+	{Impressions: 4785, Clicks: 1432, Conversions: 234},
+	{Impressions: 4858211, Clicks: 38114, Conversions: 345},
+	{Impressions: 79123, Clicks: 17274, Conversions: 542},
+}
+
 func main() {
 	fmt.Println("Hello, world!")
 }
